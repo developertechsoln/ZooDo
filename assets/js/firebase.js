bc84225cf900edf95c7ce8fbbf4f223addecfd3f
Shrey Patel <shreypdev@gmail.com>
2019-01-19 03:49:47 -0800

    configured firebase
    added script tag to firebase in page

---
+++ assets/js/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDV6lpQd6XkRNwSkHPXFJZAhXIz6f2xpcM",
+    authDomain: "zoodo-5c2b4.firebaseapp.com",
+    databaseURL: "https://zoodo-5c2b4.firebaseio.com",
+    projectId: "zoodo-5c2b4",
+    storageBucket: "zoodo-5c2b4.appspot.com",
+    messagingSenderId: "721586643525"
+};
+firebase.initializeApp(config);
