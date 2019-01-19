360143a4f6fd02d5dc5db6246d956a3045879cba
developertechsoln <46839837+developertechsoln@users.noreply.github.com>
2019-01-19 03:27:14 -0800

    Create register.js

---
+++ assets/js/register.js
@@ -0,0 +1,14 @@
+var firstName = $( "#firstName" ).val();
+var lastName = $( "#lastName" ).val();
+var email = $( "#email" ).val();
+var password = $( "#password" ).val();
+var confirmPassword = $( "#confirmPassword" ).val();
+var address = $( "#address" ).val();
+var city = $( "#city" ).val();
+var state = $( "#state" ).val();
+var country = $( "#country" ).val();
+var zipCode = $( "#zipCode" ).val();
+var phoneNumber = $( "#phoneNumber" ).val();
+
+
+
