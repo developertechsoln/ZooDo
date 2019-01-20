13de28487664dcf58623c05da179a8c89f782ca9
Shrey Patel <shreypdev@gmail.com>
2019-01-20 01:36:58 -0800

    Update register.js

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,19 +1,3 @@
-var firstName = $( "#firstName" ).val();
-var lastName = $( "#lastName" ).val();
-var email = $( "#email" ).val();
-var password = $( "#password" ).val();
-var confirmPassword = $( "#confirmPassword" ).val();
-var address = $( "#address" ).val();
-var city = $( "#city" ).val();
-var state = $( "#state" ).val();
-var country = $( "#country" ).val();
-var zipCode = $( "#zipCode" ).val();
-var phoneNumber = $( "#phoneNumber" ).val();
-var customCheckRegister = $( "#customCheckRegister" ).val();
-
-$("#createAccount").Click({
-
-});
 
 
 
