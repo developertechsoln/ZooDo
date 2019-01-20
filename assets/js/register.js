77811006ee5f828ad8850b90bc8e35f3c2e592ae
developertechsoln <46839837+developertechsoln@users.noreply.github.com>
2019-01-20 01:41:48 -0800

    update register.js
    done

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,4 +1,22 @@
+$("#createAccount").click(function(){
+    var firstName = $( "#firstName" ).val();
+var lastName = $( "#lastName" ).val();
+var email = $( "#email" ).val();
+var password = $( "#password" ).val();
+var confirmPassword = $( "#confirmPassword" ).val();
+var address = $( "#address" ).val();
+var city = $( "#city" ).val();
+var state = $( "#state" ).val();
+var country = $( "#country" ).val();
+var zipCode = $( "#zipCode" ).val();
+var phoneNumber = $( "#phoneNumber" ).val();
+var customCheckRegister = $( "#customCheckRegister" ).val();
 
-
-
-
+    var createNewUserPromise = firebase.auth().createUserWithEmailAndPassword(email, password);
+    createNewUserPromise.then(function(uid){
+       console.log(uid); 
+    });
+    createNewUserPromise.catch(function(error){
+        console.log(error);
+    });
+});
