56457b635eb604a097df87575a9044373c60dcb4
Shrey Patel <shreypdev@gmail.com>
2019-01-27 03:39:57 -0800

    Updated register.js and register.html
    I added contact number input box in html and sent that info to firebase too.
    I also fixed the bug for registration fail.

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,24 +1,27 @@
+//Onclick event listner for REGISTER button
 $("#createAccount").click(function(){
+    //Getting value from page
     var firstName = $( "#firstName" ).val();
-var lastName = $( "#lastName" ).val();
-var email = $( "#email" ).val();
-var password = $( "#password" ).val();
-var confirmPassword = $( "#confirmPassword" ).val();
-var address = $( "#address" ).val();
-var city = $( "#city" ).val();
-var state = $( "#state" ).val();
-var country = $( "#country" ).val();
-var zipCode = $( "#zipCode" ).val();
-//var phoneNumber = $( "#phoneNumber" ).val();
-var customCheckRegister = $( "#customCheckRegister" ).val();
+    var lastName = $( "#lastName" ).val();
+    var email = $( "#email" ).val();
+    var password = $( "#password" ).val();
+    var confirmPassword = $( "#confirmPassword" ).val();
+    var address = $( "#address" ).val();
+    var city = $( "#city" ).val();
+    var state = $( "#state" ).val();
+    var country = $( "#country" ).val();
+    var zipCode = $( "#zipCode" ).val();
+    var contactNumber = $( "#contactNumber" ).val();
+    var customCheckRegister = $( "#customCheckRegister" ).val();
 
+    //create promise for authenticating user
     var createNewUserPromise = firebase.auth().createUserWithEmailAndPassword(email, password);
-    
     createNewUserPromise.then(function(){
-        firebase.auth().onAuthStateChanged(function(user){
+        firebase.auth().onAuthStateChanged(function(user){ //check the auth state of user to get uid of user
             if (user){
-                 var userid = user.uid;
-                var saveUserPromise = firebase.database().ref().child("Data").child("userInfo").child(userid).set({
+                var userid = user.uid;
+                //create promise for sending user data to database
+                var saveUserPromise = firebase.database().ref().child("data").child("tempUserInfo").child(userid).set({
                         firstName: firstName,
                         lastName :lastName,
                         email: email,
@@ -26,21 +29,27 @@ var customCheckRegister = $( "#customCheckRegister" ).val();
                         city: city,
                         state: state , 
                         country: country,
-                        zipCode: zipCode
+                        zipCode: zipCode,
+                        contactNumber: contactNumber
                 });
                 saveUserPromise.then(function(){
                     console.log("done");
                 });
                 saveUserPromise.catch(function(error){
-                    console.log("OOPS! some error occured");
-                    
+                    //creating promise to delete user if we auth user but was not able to store user info in database
+                    var userDeletePromise = firebase.auth().currentUser.delete();
+                    userDeletePromise.then(function() {
+                        console.log("Please try again, sorry but we are currently not able to sign you up.");
+                    });
+                    userDeletePromise.catch(function(error) {
+                        console.log("Oops!! Something went wrong please contact office.")
+                    });
                 });
             } 
 
         });
     
     });
-
     createNewUserPromise.catch(function(error){
             console.log(error.message);
     });
