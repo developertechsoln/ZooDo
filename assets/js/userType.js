f274ab46fae71b45d4503071954b4ce7b96af9da
developertechsoln <46839837+developertechsoln@users.noreply.github.com>
2019-01-28 22:55:52 -0800

    Update userType.js

--- assets/js/userType.js
+++ assets/js/userType.js
@@ -33,6 +33,39 @@ $("#employer").click(function(){
     })
 })
 
+
 $("#employee").click(function(){
-    
+    firebase.auth().onAuthStateChanged(function(user){
+        if (user){
+            var userId = user.uid;
+            var globalUserInfoPromise = firebase.database().ref().child("data").child("userInfo").child(userId).set("employee");
+            globalUserInfoPromise.then(function(){
+                var dbRefTempUserInfo = firebase.database().ref().child("data").child("tempUserInfo").child(userId);
+                dbRefTempUserInfo.once("value", snap => {
+                    if (snap.exists()){
+                        var userInfo = snap.val();
+                        var deleteTempUserInfoPromise = dbRefTempUserInfo.remove();
+                        deleteTempUserInfoPromise.then(function(){
+                            var storeUserInfoInEmployeePromise = firebase.database().ref().child("data").child("employee").child("userInfo").child(userId).set(userInfo);
+                            storeUserInfoInEmployeePromise.then(function(){
+                                console.log("DONE!");
+                            });
+                            storeUserInfoInEmployeePromise.catch(function(error){
+                                console.log(error.message)
+                            })
+                        });
+                        deleteTempUserInfoPromise.catch(function(error){
+                            console.log(error.message);
+                        })
+                    } else {
+                        console.log("Try Again")
+                    }
+                })
+            });
+            globalUserInfoPromise.catch(function(error){
+                console.log(error.message)
+            });
+        }
+    })
 })
+
