81c361c3b62925ee9bdf38ccaf92abe8a1aed2bf
Shrey <shreypdev@gmail.com>
2019-02-02 02:16:00 -0800

    update create_user.js
    added comments

--- assets/js/create_profile.js
+++ assets/js/create_profile.js
@@ -1,7 +1,11 @@
+//This function is called when "+" button is pressed.
+//This function will add extra educatio information form for a user
 $("#add-extra-education").click(function() {
+    //this line will check in div block of html, how many form already exsists
     var educationInfoNumber = $("#extra-education").children().length + 2;
+    //this is dynamic addition of content to html from js
     $("#extra-education").append(
-        "<div id=\"extra-education-" + educationInfoNumber + "\">" +
+        "<div id=\"extra-education-" + educationInfoNumber + "\">" + //adding different id for every info form so we can delete it
             "<br>"+
             "<h6 class=\"heading mb-4 text-center text-primary\">Education Information " + educationInfoNumber + "</h6>"+
             "<div class=\"pl-lg-4\">"+
@@ -47,18 +51,23 @@ $("#add-extra-education").click(function() {
         "</div>"
     );
 
+    //we make remove info form button visible if we have any extra info form available
     if($("#extra-education").children().length > 0){
         document.getElementById("remove-extra-education").removeAttribute("style");
         document.getElementById("remove-extra-education").setAttribute("style", "display: inline-block;");
     }
 });
 
+//this function is called when remove extra eduacation info button is pressed 
 $("#remove-extra-education").click(function() {
+    //check total number of extra education info forms and making id name to remove last form
     var totalNumberOfEducationRecord = $("#extra-education").children().length + 1;
     var idName = "#extra-education-" + totalNumberOfEducationRecord;
 
+    //remove last extra education form
     $(idName).remove();
 
+    //check if their is any extra education form if no then remove button which was added to hide extra education form
     if($("#extra-education").children().length == 0) {
         document.getElementById("remove-extra-education").removeAttribute("style");
         document.getElementById("remove-extra-education").setAttribute("style", "display: none;");
