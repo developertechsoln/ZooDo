7808473def4d0d7ff0e536539cfeeb869fe2cc08
Shrey <shreypdev@gmail.com>
2019-02-02 03:49:45 -0800

    added new feature in create_profile, now you can add education also

--- assets/js/create_profile.js
+++ assets/js/create_profile.js
@@ -73,3 +73,81 @@ $("#remove-extra-education").click(function() {
         document.getElementById("remove-extra-education").setAttribute("style", "display: none;");
     }
 });
+
+
+//This function is called when "+" button is pressed.
+//This function will add extra work information form for a user
+$("#add-extra-work-experience").click(function() {
+    //this line will check in div block of html, how many form already exsists
+    var workInfoNumber = $("#extra-work-experience").children().length + 2;
+    //this is dynamic addition of content to html from js
+    $("#extra-work-experience").append(
+        "<div id=\"extra-work-experience-" + workInfoNumber + "\">" + //adding different id for every info form so we can delete it
+            "<br>"+
+            "<h6 class=\"heading-small text-muted mb-4\">Work Experience</h6>"+
+            "<div id=\"work-experience-1\">"+
+                "<h6 class=\"heading mb-4 text-center text-primary\">Work Experience " + workInfoNumber + "</h6>"+
+                "<div class=\"pl-lg-4\">"+
+                    "<div class=\"row\">"+
+                    "<div class=\"col-lg-5\">"+
+                        "<div class=\"form-group\">"+
+                        "<label class=\"form-control-label\">Company Name</label>"+
+                        "<input type=\"text\" id=\"\" class=\"form-control form-control-alternative\" placeholder=\"Company Name\">"+
+                        "</div>"+
+                    "</div>"+
+                    "<div class=\"col-lg-5\">"+
+                        "<div class=\"form-group\">"+
+                        "<label class=\"form-control-label\">Job Title</label>"+
+                        "<input type=\"text\" id=\"\" class=\"form-control form-control-alternative\" placeholder=\"Job Title\">"+
+                        "</div>"+
+                    "</div>"+
+                    "<div class=\"col-lg-2\">"+
+                        "<div class=\"form-group\">"+
+                        "<label class=\"form-control-label\">Years</label>"+
+                        "<select class=\"form-control form-control-alternative\">"+
+                            "<option value=\"less-then-1-year\">< a year</option>"+
+                            "<option value=\"1-year\">1 year</option>"+
+                            "<option value=\"2-years\">2 years</option>"+
+                            "<option value=\"3-years\">3 years</option>"+
+                            "<option value=\"4-years\">4 years</option>"+
+                            "<option value=\"5-years\">5 years</option>"+
+                            "<option value=\"more-than-5-years\">> 5 years</option>"+
+                        "</select>"+
+                        "</div>"+
+                    "</div>"+
+                    "</div>"+
+                    "<div class=\"row\">"+
+                    "<div class=\"col-lg-12\">"+
+                        "<div class=\"form-group\">"+
+                        "<label class=\"form-control-label\">Job Discription</label>"+
+                        "<textarea rows=\"4\" class=\"form-control form-control-alternative\" placeholder=\"Discribe your work in few words ...\"></textarea>"+
+                        "</div>"+
+                    "</div>"+
+                    "</div>"+
+                "</div>"+
+            "</div>"+
+        "</div>"
+    );
+
+    //we make remove info form button visible if we have any extra info form available
+    if($("#extra-work-experience").children().length > 0){
+        document.getElementById("remove-extra-work-experience").removeAttribute("style");
+        document.getElementById("remove-extra-work-experience").setAttribute("style", "display: inline-block;");
+    }
+});
+
+//this function is called when remove extra work info button is pressed 
+$("#remove-extra-work-experience").click(function() {
+    //check total number of extra education info forms and making id name to remove last form
+    var totalNumberOfWorkExperienceRecord = $("#extra-work-experience").children().length + 1;
+    var idName = "#extra-work-experience-" + totalNumberOfWorkExperienceRecord;
+
+    //remove last extra education form
+    $(idName).remove();
+
+    //check if their is any extra education form if no then remove button which was added to hide extra education form
+    if($("#extra-work-experience").children().length == 0) {
+        document.getElementById("remove-extra-work-experience").removeAttribute("style");
+        document.getElementById("remove-extra-work-experience").setAttribute("style", "display: none;");
+    }
+});
