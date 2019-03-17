d943479cb2092d2edd55bbdcf4d8c64ab90c05ad
iamutkarsh23 <32732488+iamutkarsh23@users.noreply.github.com>
2019-03-16 20:22:58 -0700

    [979528]: Utkarsh: Temp - JSON to be expected.

--- assets/js/view_profile.js
+++ assets/js/view_profile.js
@@ -12,3 +12,97 @@ $(function viewIntroduction(var introductionJson){
         console.log('error');
     });
 });
+
+
+/* Expected json
+
+{
+    profileInfo: 
+    {
+        Introduction: 
+        {
+            firstName: "XYZ",
+            lastName: "ABC",
+            title: "Software Engineer @ Microsoft",
+            profilePictureUrl: "https://example.com/qwerty/1.png"
+        }
+
+        Education: 
+        {
+            education1:
+            {
+                schoolName: "UVIC",
+                degreeType: "Bachelor",
+                courseName: "Computer Science",
+                dateOfGraduation: "03-31-2017"
+            }
+            education2:
+            {
+                schoolName: "UBC",
+                degreeType: "Bachelor",
+                courseName: "Economics",
+                dateOfGraduation: "03-31-2027"
+            }
+        }
+
+        workExperience:
+        {
+            workExperience1:
+            {
+                companyName:"Microsoft",
+                jobTitle: "QA",
+                years: "5",
+                description: "I worked for testing team."
+            }
+            workExperience2:
+            {
+                companyName:"Google",
+                jobTitle: "Software Engineer",
+                years: "2",
+                description: "I worked for google assistance team."
+            }
+        }
+
+        skills:
+        {
+            skill1:
+            {
+                skillName:"Java",
+                skillDescription: "I am working with Java since 10 years."
+            }
+            skill2:
+            {
+                skillName:"Python",
+                skillDescription: "I am working with Java since 1 year."
+            }
+        }
+    }
+
+    profileMedia:
+    {
+        photo1: 
+        {
+            pathInFirestore: "photo/uid",
+            photoUrl: "https://example.com/qwerty/1.png",
+            photoDescription: "My first work."
+        }
+        photo2: 
+        {
+            pathInFirestore: "photo/uid",
+            photoUrl: "https://example.com/qwerty/2.png",
+            photoDescription: "My second work."
+        }
+        video1:
+        {
+            pathInFirestore: "photo/uid",
+            video1Url: "https://example.com/qwerty/1.mp4"
+        }
+        video2:
+        {
+            pathInFirestore: "photo/uid"
+            video2Url: "https://example.com/qwerty/2.mp4"
+        }
+    }
+    
+}
+*/    
